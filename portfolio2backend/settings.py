c77015741071e8476055fa8b0a133be2878e89dc
Arunarchitect <intformbase@gmail.com>
2024-08-30 15:04:59 +0530

    added cors headers

--- portfolio2backend/settings.py
+++ portfolio2backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l(%_5!4_x7x^&h8laem=#&^z1k72t19-56r4=&2zllp98+=f&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    "corsheaders",
     'rest_framework',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +55,16 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'portfolio2backend.urls'
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:9000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 TEMPLATES = [
     {
